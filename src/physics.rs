105f97a1e977b3a4f67972af53e547df73de02b4
Charalampos Mitrodimas <charmitro@gmail.com>
2021-08-17 16:45:10 +0300

    add todo

--- src/physics.rs
+++ src/physics.rs
@@ -4,36 +4,38 @@ use crate::components::*;
 
 pub struct Physics;
 
-// 800, 600
-fn check_window_collision(pos: Position, direction: Direction) -> bool {
-    use self::Direction::*;
-    match direction {
-        Left => {
-            if pos.0.x() < ((-400) + 20) {
-                return false;
-            } else {
-                return true;
+// TODO find a way to get window size dynamically
+impl Physics {
+    fn check_window_collision(pos: Position, direction: Direction) -> bool {
+        use self::Direction::*;
+        match direction {
+            Left => {
+                if pos.0.x() < ((-400) + 20) {
+                    return false;
+                } else {
+                    return true;
+                }
             }
-        }
-        Right => {
-            if pos.0.x() < ((400) - 20) {
-                return true;
-            } else {
-                return false;
+            Right => {
+                if pos.0.x() < ((400) - 20) {
+                    return true;
+                } else {
+                    return false;
+                }
             }
-        }
-        Up => {
-            if pos.0.y() > ((-300) + 20) {
-                return true;
-            } else {
-                return false;
+            Up => {
+                if pos.0.y() > ((-300) + 20) {
+                    return true;
+                } else {
+                    return false;
+                }
             }
-        }
-        Down => {
-            if pos.0.y() < ((300) - 20) {
-                return true;
-            } else {
-                return false;
+            Down => {
+                if pos.0.y() < ((300) - 20) {
+                    return true;
+                } else {
+                    return false;
+                }
             }
         }
     }
@@ -48,22 +50,22 @@ impl<'a> System<'a> for Physics {
         for (pos, vel) in (&mut data.0, &data.1).join() {
             match vel.direction {
                 Left => {
-                    if check_window_collision(pos.clone(), vel.direction) {
+                    if Self::check_window_collision(pos.clone(), vel.direction) {
                         pos.0 = pos.0.offset(-vel.speed, 0);
                     }
                 }
                 Right => {
-                    if check_window_collision(pos.clone(), vel.direction) {
+                    if Self::check_window_collision(pos.clone(), vel.direction) {
                         pos.0 = pos.0.offset(vel.speed, 0);
                     }
                 }
                 Up => {
-                    if check_window_collision(pos.clone(), vel.direction) {
+                    if Self::check_window_collision(pos.clone(), vel.direction) {
                         pos.0 = pos.0.offset(0, -vel.speed);
                     }
                 }
                 Down => {
-                    if check_window_collision(pos.clone(), vel.direction) {
+                    if Self::check_window_collision(pos.clone(), vel.direction) {
                         pos.0 = pos.0.offset(0, vel.speed);
                     }
                 }
